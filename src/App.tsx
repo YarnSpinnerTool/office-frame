f5b69bf5fb843c83ac9e8c461bb046607bbe1d86
Jon Manning <jon@secretlab.com.au>
2024-08-05 18:44:30 +1000

    Update fonts and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 function App() {
+
+  const now = new Date();
+
   return (
     <>
-      <div className="content">
-        <div style={{display: "flex", padding: "20px"}}>
-          {"Hello, world!".repeat(4000)}
-        </div>
+      <div className="content" style={{display: "flex", flexDirection: "column"}}>
+          <div style={{height: "2rem", borderBottom: "2px solid var(--black)"}}>
+            Welcome to <strong>Yarn Spinner</strong>
+          </div>
+          <div style={{flexGrow: 1, overflow: "hidden"}}>
+            {"Hello, world! ".repeat(4000)}
+            </div>
+          <div style={{height: "2rem", borderTop: "2px solid var(--black)", display:"flex", justifyContent: "space-between"}}>
+          <div></div>
+          <div>Updated: {now.toLocaleString("en-au", {
+            dateStyle: "long",
+            timeStyle: "short"
+            
+          })}</div>
+          </div>
+        
       </div>
     </>
   )
